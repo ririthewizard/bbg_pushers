b75eef3ce136c04fb80342de7bdf1d470edc9c00
ririthewizard <ririwilliams.rw@gmail.com>
2025-03-18 11:22:17 -0500

    Well, closer now. Just have to work at it a bit more

--- bot.py
+++ bot.py
@@ -1,18 +1,24 @@
 import os
 import sys
 import interactions
-from dotenv import find_dotenv, load_dotenv
+from dotenv import dotenv_values, find_dotenv, load_dotenv
 from stravalib import Client
 
-load_dotenv(find_dotenv())
+config = dotenv_values(".env")
 
-strava_client_id = os.getenv("STRAVA_CLIENT_ID")
-strava_client_secret = os.getenv("STRAVA_CLIENT_SECRET")
-strava_client = Client()
+strava_client_id = 152444
+strava_client_secret = str(config["STRAVA_CLIENT_SECRET"])
+strava_client = Client() 
 url = strava_client.authorization_url(
     client_id=152444,
-    redirect_uri="https://github.com/ririthewizard/bbg_pushers/"
+    redirect_uri="http://localhost/authorization",
 )
+
+token_response = strava_client.exchange_code_for_token(
+    client_id=strava_client_id,
+    client_secret=strava_client_secret,
+    code="7251c81f7ea195db7895030874b3c5fc4a2fcfed")
+
 
 guild_id = os.getenv("GUILD_ID")
 bot_token = os.getenv("BOT_TOKEN")
