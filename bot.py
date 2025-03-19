f402b66c15577852e92a1bd797312d978425829d
ririthewizard <ririwilliams.rw@gmail.com>
2025-03-19 16:16:23 -0500

    I dunno. I really can't figure out what is going wrong here. Getting a 400 bad request error but I just fetched a fresh code for my athlete acc. Not sure where to go from here

--- bot.py
+++ bot.py
@@ -1,34 +1,48 @@
 import os
 import sys
+from dotenv.main import dotenv_values
 import interactions
-from dotenv import dotenv_values, find_dotenv, load_dotenv
 from stravalib import Client
 
 config = dotenv_values(".env")
 
-strava_client_id = 152444
-strava_client_secret = str(config["STRAVA_CLIENT_SECRET"])
+STRAVA_CLIENT_ID = int(str(config["STRAVA_CLIENT_ID"]))
+STRAVA_CLIENT_SECRET = str(config["STRAVA_CLIENT_SECRET"])
 strava_client = Client() 
+
+print(STRAVA_CLIENT_ID)
+
+if strava_client is None:
+    print(ValueError("Strava client empty")) 
+
+#strava_client.refresh_access_token(STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET, "f890f0cdb8c1b8c2a93eae4bfeb668f31b9d4d41")
+
 url = strava_client.authorization_url(
-    client_id=152444,
-    redirect_uri="http://localhost/authorization",
+    client_id=STRAVA_CLIENT_ID,
+    redirect_uri="http://127.0.0.1/authorization",
 )
-
 token_response = strava_client.exchange_code_for_token(
-    client_id=strava_client_id,
-    client_secret=strava_client_secret,
-    code="7251c81f7ea195db7895030874b3c5fc4a2fcfed")
+    client_id=STRAVA_CLIENT_ID,
+    client_secret=STRAVA_CLIENT_SECRET,
+
+    code="174702ccc769b96954a40c83bc4231e85fce1b84"
+)
+access_token = token_response["access_token"]
+refresh_token = token_response['refresh_token']
+
+athlete = strava_client.get_athlete()
+print(f"{athlete.firstname}'s token expires at {token_response}")
 
 
-guild_id = os.getenv("GUILD_ID")
-bot_token = os.getenv("BOT_TOKEN")
-bot = interactions.Client(intents=interactions.Intents.DEFAULT) 
+GUILD_ID = config["GUILD_ID"]
+BOT_TOKEN = config["BOT_TOKEN"]
+
+bot = interactions.Client(intents=interactions.Intents.DEFAULT)
 
 @interactions.listen()
 async def on_ready():
-    print("Ready")
+    print("ready")
     print(f"Logged in as {bot.user}")
-
 @interactions.listen()
 async def on_message_create(event):
     print(f"Message received: {event.message.jump_url}")
@@ -40,5 +54,5 @@ async def howdy_command(ctx: interactions.SlashContext):
 
 
 print(url)
-bot.start(bot_token)
+bot.start(BOT_TOKEN)
 
