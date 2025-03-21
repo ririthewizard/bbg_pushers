0258ce716a4d4090b7fc6b0c36f7a38ee3e2cb37
ririthewizard <ririwilliams.rw@gmail.com>
2025-03-20 21:20:34 -0500

    LET'S FREAKING GO! Got it working. Now have to change the scope to give myself read permissions so I can actually access activity data, then I will implement this to display in Discord

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import os
 import sys
-from dotenv.main import dotenv_values
+import dotenv
 import interactions
 from stravalib import Client
 
-config = dotenv_values(".env")
+dotenv.load_dotenv()
 
-STRAVA_CLIENT_ID = int(str(config["STRAVA_CLIENT_ID"]))
-STRAVA_CLIENT_SECRET = str(config["STRAVA_CLIENT_SECRET"])
-strava_client = Client() 
+STRAVA_CLIENT_ID = int(str(os.getenv("STRAVA_CLIENT_ID")))
+STRAVA_CLIENT_SECRET = str(os.getenv("STRAVA_CLIENT_SECRET"))
+strava_client = Client(
+    access_token=str(os.getenv("ACCESS_TOKEN")),
+    refresh_token=str(os.getenv("REFRESH_TOKEN")),
+    token_expires=int(str(os.getenv("EXPIRES_AT")))
+) 
 
 print(STRAVA_CLIENT_ID)
 
@@ -16,11 +20,12 @@ if strava_client is None:
     print(ValueError("Strava client empty")) 
 
 #strava_client.refresh_access_token(STRAVA_CLIENT_ID, STRAVA_CLIENT_SECRET, "f890f0cdb8c1b8c2a93eae4bfeb668f31b9d4d41")
-
 url = strava_client.authorization_url(
     client_id=STRAVA_CLIENT_ID,
     redirect_uri="http://127.0.0.1/authorization",
+    scope=["read_all"]
 )
+'''
 token_response = strava_client.exchange_code_for_token(
     client_id=STRAVA_CLIENT_ID,
     client_secret=STRAVA_CLIENT_SECRET,
@@ -29,13 +34,14 @@ token_response = strava_client.exchange_code_for_token(
 )
 access_token = token_response["access_token"]
 refresh_token = token_response['refresh_token']
+'''
 
 athlete = strava_client.get_athlete()
-print(f"{athlete.firstname}'s token expires at {token_response}")
+print(f"{athlete.firstname}'s token expires at {strava_client.token_expires}")
 
 
-GUILD_ID = config["GUILD_ID"]
-BOT_TOKEN = config["BOT_TOKEN"]
+GUILD_ID = str(os.getenv("GUILD_ID"))
+BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 
 bot = interactions.Client(intents=interactions.Intents.DEFAULT)
 
@@ -51,8 +57,14 @@ async def on_message_create(event):
 @interactions.slash_command(name="howdy", description="Howdy howdy howdy!", scopes=[1117995178171564143])
 async def howdy_command(ctx: interactions.SlashContext):
     await ctx.send("Howdy!")
+'''
+riley_activities = strava_client.get_activities(after="2025-01-01", limit=10)
 
+array_of_activities = []
+for activity in riley_activities:
+    array_of_activities.append(activity)
 
+print(f"Most recent activity: {array_of_activities[:-1]}")
+'''
 print(url)
 bot.start(BOT_TOKEN)
-
